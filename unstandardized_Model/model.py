e041ad80f02ba11e6f42d2495924fa4a2e11bc8b
CerealSurya <sjalapati42@gmail.com>
2023-12-29 14:59:20 -0800

    changed fromLogits

--- unstandardized_Model/model.py
+++ unstandardized_Model/model.py
@@ -53,7 +53,7 @@ outputs = prediction_layer(x) #Get our outputs from the fully connected layer we
 model = tf.keras.Model(inputs, outputs)
 print(model.summary())
 base_learning_rate = 0.0001
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),loss=tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics=[tf.keras.metrics.BinaryAccuracy(threshold=0, name='accuracy')])
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),loss=tf.keras.losses.BinaryCrossentropy(from_logits=False), metrics=[tf.keras.metrics.BinaryAccuracy(threshold=0, name='accuracy')])
 path = 'data/testing'
 
 csv_logger = CSVLogger('log.csv', append=True, separator=';')
