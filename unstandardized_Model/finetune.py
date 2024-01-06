763a87750545bc08c8abaa405fcaaa6f5d01a3ed
CerealSurya <sjalapati42@gmail.com>
2024-01-06 09:27:39 -0800

    modified finetune procedure

--- unstandardized_Model/finetune.py
+++ unstandardized_Model/finetune.py
@@ -6,9 +6,9 @@ from tensorflow.keras import optimizers
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator 
 from tensorflow.python.keras.optimizer_v2.rmsprop import RMSprop 
+#For the second transfer we reconstruct this same model using the trainedInception.keras as the base training only from like 270 onwards
 
-model = tf.keras.models.load_model('machine.keras')
-print(model.summary())
+oldModel = tf.keras.models.load_model('machine.keras')
 
 BATCH_SIZE = 8
 IMG_SIZE = (299, 299)
@@ -16,19 +16,46 @@ train_dir = "initialDataset"
 validation_dir = "initialDataset/validation"
 train_dataset = tf.keras.utils.image_dataset_from_directory(train_dir, shuffle=True, batch_size=BATCH_SIZE, image_size=IMG_SIZE)
 validation_dataset = tf.keras.utils.image_dataset_from_directory(validation_dir, shuffle=True, batch_size=BATCH_SIZE, image_size=IMG_SIZE)
-#!Might have to rethink how we do the fine tuning depending on how many layers there are
-print("Number of layers in the base model: ", len(model.layers)) # --> ~320 or 8
+preprocess_input = tf.keras.applications.inception_v3.preprocess_input
 
+base_model = tf.keras.applications.inception_v3.InceptionV3(input_shape= IMG_SIZE + (3,), include_top=False, weights='imagenet')
+print(len(base_model.layers))
 # Fine-tune from this layer onwards
 fine_tune_at = 220
-
 # Freeze all the layers before the `fine_tune_at` layer
-for layer in model.layers[:fine_tune_at]:
+for layer in base_model.layers[:fine_tune_at]:
   layer.trainable = False
 
+#https://www.tensorflow.org/tutorials/images/transfer_learning
+image_batch, label_batch = next(iter(train_dataset)) #Utilizes keras api to get the images and labels separated
+feature_batch = base_model(image_batch) # Gets the features (inputs of the layers) of the model from passing in the images
+global_average_layer = tf.keras.layers.GlobalAveragePooling2D() 
+feature_batch_average = global_average_layer(feature_batch) #Pools the features together to simplify them to get an output. Helps simplify network size and computing. Precursor to fully connected layer
+#prediction_layer = tf.keras.layers.Dense(1, activation="sigmoid") #Fully connected layer, getting prediction
+prediction_layer = tf.keras.models.Sequential(oldModel.layers[len(oldModel.layers) - 1])
+
+data_augmentation = tf.keras.Sequential([
+  tf.keras.layers.RandomFlip('horizontal'),
+  tf.keras.layers.RandomRotation(0.2),
+])
+
+inputs = tf.keras.Input(shape=(299, 299, 3))
+#Our defined model with the steps in sequential order
+x = data_augmentation(inputs) #Augment all of the inputs
+x = preprocess_input(x) #Process all of the inputs. Manipulating their size and color values to fit requirements of the model
+
+x = base_model(x, training=False) #Pass in the inputs without manipulating weight values
+x = global_average_layer(x) #Precursor to fully connected layer (Pools everything together)
+x = tf.keras.layers.Dropout(0.2)(x)
+outputs = prediction_layer(x) #Get our outputs from the fully connected layer we defined above
+
+model = tf.keras.Model(inputs, outputs)
+print(model.summary())
 base_learning_rate = 0.0001
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),loss=tf.keras.losses.BinaryCrossentropy(from_logits=False), metrics=[tf.keras.metrics.BinaryAccuracy(threshold=0, name='accuracy')])
+
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),loss=tf.keras.losses.BinaryCrossentropy(from_logits=False), metrics=[tf.keras.metrics.BinaryAccuracy(threshold=0, name='accuracy'), tf.keras.metrics.AUC(name="AUC")])
 path = 'data/testing'
 
-model.fit(train_dataset, epochs=10, validation_data=validation_dataset)
+model.fit(train_dataset, epochs=4, validation_data=validation_dataset)
+base_model.save('fineTunedBase.keras')
 model.save('machineFineTune.keras')
--- unstandardized_Model/model.py
+++ unstandardized_Model/model.py
@@ -24,6 +24,7 @@ preprocess_input = tf.keras.applications.inception_v3.preprocess_input
 
 
 base_model = tf.keras.applications.inception_v3.InceptionV3(input_shape= IMG_SIZE + (3,), include_top=False, weights='imagenet')
+print(len(base_model.layers))
 base_model.trainable = False
 
 #https://www.tensorflow.org/tutorials/images/transfer_learning
@@ -57,19 +58,6 @@ model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rat
 path = 'data/testing'
 
 csv_logger = CSVLogger('log.csv', append=True, separator=';')
-model.fit(train_dataset, epochs=2, callbacks=[csv_logger], validation_data=validation_dataset, shuffle=True)
+model.fit(train_dataset, epochs=1, callbacks=[csv_logger], validation_data=validation_dataset, shuffle=True)
 
-
-# #----------------------- Fine Tuning
-base_model.trainable = True
-
-# Fine-tune from this layer onwards
-fine_tune_at = 220
-
-# Freeze all the layers before the `fine_tune_at` layer
-for layer in base_model.layers[:fine_tune_at]:
-  layer.trainable = False
-model.fit(train_dataset, epochs=10, callbacks=[csv_logger], validation_data=validation_dataset)
-base_model.save('trainedInception.keras')
-model.save('machineFineTuned.keras')
-#For the second transfer we reconstruct this same model using the trainedInception.keras as the base training only from like 270 onwards
+model.save('machine.keras')
