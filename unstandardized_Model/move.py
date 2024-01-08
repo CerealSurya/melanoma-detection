261c8cb9016e5d073c83141156a42ca63505a8c6
CerealSurya <sjalapati42@gmail.com>
2024-01-08 12:50:37 -0800

    bug fixes to move

--- unstandardized_Model/move.py
+++ unstandardized_Model/move.py
@@ -3,22 +3,23 @@ import os
 #GOAL: Move 1000 imgs from training to create validation data
 num = 0
 zeros = "000"
-for i in range(500):
+for i in range(10):
+    print(num)
     if num >= 10 and num < 100:
         zeros = "00"
     elif num >= 100:
         zeros = "0"
     try:
-        os.rename(f"/initialDataset/train/lesion_Present/mn500/mn500/mn500/seed{zeros}{num}", f"/initialDataset/validation/lesion_Present/seed{zeros}{num}")
+        os.rename(f"./initialDataset/train/lesion_Present/mn500/mn500/mn500/seed{zeros}{num}.png", f"./initialDataset/validation/lesion_Present/seed{zeros}{num}.png")
     except:
-        print(f"error occured on img seed{num}")
+        print(f"error occured on img seed{zeros}{num}")
     num+=1
 
 num = 0
 counter = 0
 while num < 500:
     try:
-        os.rename(f"/initialDataset/train/lesion_NotPresent/seed{counter}", f"/initialDataset/validation/lesion_NotPresent/seed{counter}")
+        os.rename(f"./initialDataset/train/lesion_NotPresent/seed{counter}.png", f"./initialDataset/validation/lesion_NotPresent/seed{counter}.png")
     except:
         num-=1
         print(f"error occured on img seed{num}")
