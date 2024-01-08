1553920dfeca60b44e6b765da440ba3a9ad08298
CerealSurya <sjalapati42@gmail.com>
2024-01-08 12:42:45 -0800

    added script to create validation split

---
+++ unstandardized_Model/move.py
@@ -0,0 +1,26 @@
+import os
+
+#GOAL: Move 1000 imgs from training to create validation data
+num = 0
+zeros = "000"
+for i in range(500):
+    if num >= 10 and num < 100:
+        zeros = "00"
+    elif num >= 100:
+        zeros = "0"
+    try:
+        os.rename(f"/initialDataset/train/lesion_Present/mn500/mn500/mn500/seed{zeros}{num}", f"/initialDataset/validation/lesion_Present/seed{zeros}{num}")
+    except:
+        print(f"error occured on img seed{num}")
+    num+=1
+
+num = 0
+counter = 0
+while num < 500:
+    try:
+        os.rename(f"/initialDataset/train/lesion_NotPresent/seed{counter}", f"/initialDataset/validation/lesion_NotPresent/seed{counter}")
+    except:
+        num-=1
+        print(f"error occured on img seed{num}")
+    num+=1
+    counter+=1
