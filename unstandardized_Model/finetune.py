801a611cf22e5588a366eb8cbc37ea5a599c09fb
CerealSurya <sjalapati42@gmail.com>
2024-01-15 11:07:37 -0800

    reduced batch size again to 1

--- unstandardized_Model/finetune.py
+++ unstandardized_Model/finetune.py
@@ -12,7 +12,7 @@ from tensorflow.python.keras.optimizer_v2.rmsprop import RMSprop
 oldModel = tf.keras.models.load_model('machineVGG.h5')
 
 
-BATCH_SIZE = 2
+BATCH_SIZE = 1
 IMG_SIZE = (224, 224)
 train_dir = "initialDataset/train"
 validation_dir = "initialDataset/validation"
