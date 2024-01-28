0cc7a04f0dbb2103d354b6dec753b7eabd23fa3a
CerealSurya <sjalapati42@gmail.com>
2024-01-27 18:52:25 -0800

    bug fixes on splitting

--- ISIC_Model/splitISIC.py
+++ ISIC_Model/splitISIC.py
@@ -7,7 +7,7 @@ df = df.reset_index()
 
 "Splitting training data into benign / malignant"
 for i in df.index: 
-    image = df.loc[i].at['image_id'] + '.jpg'
+    image = df.loc[i].at['image_name'] + '.jpg'
     try:
         status = 'benign'
         if df.loc[i].at['benign_malignant'] == 'malignant':
