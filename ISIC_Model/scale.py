3789bf029cb603f83be0c473bd130eda7ff3049e
CerealSurya <sjalapati42@gmail.com>
2024-02-17 19:27:20 -0800

    forgot about other melanoam folder

--- ISIC_Model/scale.py
+++ ISIC_Model/scale.py
@@ -1,46 +1,56 @@
 from PIL import Image
 
 from os import listdir
-path = "combinedDataset/train/benign/"
-benign = [f for f in listdir(path)]
+path = "combinedDataset/train/malignant/archive (3)/melanoma_cancer_dataset/malignant/"
+mal = [f for f in listdir(path)]
 
 imgs = ["png", "jpg", "peg"]
-for img in benign:
+for img in mal:
     if img[len(img) - 3:] not in imgs:
         continue
     image_file = Image.open(f"{path}{img}") 
 
-    image_file.save(f"newBenign/{img}", quality = 30)
+    image_file.save(f"newMal/other{img}", quality = 30)
+# path = "combinedDataset/train/benign/"
+# benign = [f for f in listdir(path)]
 
-path = "combinedDataset/train/benign/HAM10000_images_part_1/"
-benign = [f for f in listdir(path)]
+# imgs = ["png", "jpg", "peg"]
+# for img in benign:
+#     if img[len(img) - 3:] not in imgs:
+#         continue
+#     image_file = Image.open(f"{path}{img}") 
 
+#     image_file.save(f"newBenign/{img}", quality = 30)
 
-for img in benign:
-    if img[len(img) - 3:] not in imgs:
-        continue
-    image_file = Image.open(f"{path}{img}") 
-
-    image_file.save(f"newBenign/HAM{img}", quality = 30)
-
-path = "combinedDataset/train/benign/HAM10000_images_part_2/"
-benign = [f for f in listdir(path)]
-
-
-for img in benign:
-    if img[len(img) - 3:] not in imgs:
-        continue
-    image_file = Image.open(f"{path}{img}") 
-
-    image_file.save(f"newBenign/HAM{img}", quality = 30)
-
-path = "combinedDataset/train/malignant/"
-malignant = [f for f in listdir(path)]
+# path = "combinedDataset/train/benign/HAM10000_images_part_1/"
+# benign = [f for f in listdir(path)]
 
 
-for img in malignant:
-    if img[len(img) - 3:] not in imgs:
-        continue
-    image_file = Image.open(f"{path}{img}") 
+# for img in benign:
+#     if img[len(img) - 3:] not in imgs:
+#         continue
+#     image_file = Image.open(f"{path}{img}") 
 
-    image_file.save(f"newMal/{img}", quality = 30)
+#     image_file.save(f"newBenign/HAM{img}", quality = 30)
+
+# path = "combinedDataset/train/benign/HAM10000_images_part_2/"
+# benign = [f for f in listdir(path)]
+
+
+# for img in benign:
+#     if img[len(img) - 3:] not in imgs:
+#         continue
+#     image_file = Image.open(f"{path}{img}") 
+
+#     image_file.save(f"newBenign/HAM{img}", quality = 30)
+
+# path = "combinedDataset/train/malignant/"
+# malignant = [f for f in listdir(path)]
+
+
+# for img in malignant:
+#     if img[len(img) - 3:] not in imgs:
+#         continue
+#     image_file = Image.open(f"{path}{img}") 
+
+#     image_file.save(f"newMal/{img}", quality = 30)
