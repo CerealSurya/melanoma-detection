29ca2d4d33398458f36cb9e7725c44d2936ad1a1
CerealSurya <sjalapati42@gmail.com>
2024-06-02 08:33:03 -0700

    updated output

--- generator/output.py
+++ generator/output.py
@@ -5,13 +5,69 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-import sys 
+# import sys 
 
-kerasFile = sys.argv[1]
-print(f'Running evaluation on file {kerasFile}')
-model = tf.keras.models.load_model(kerasFile)
+# kerasFile = sys.argv[1]
+# print(f'Running evaluation on file {kerasFile}')
 
-model = load_model(kerasFile)
+
+def build_unet(input_shape):
+    inputs = layers.Input(shape=input_shape)
+    
+    # Encoder
+    c1 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(inputs)
+    c1 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(c1)
+    p1 = layers.MaxPooling2D((2, 2))(c1)
+    
+    c2 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(p1)
+    c2 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(c2)
+    p2 = layers.MaxPooling2D((2, 2))(c2)
+    
+    c3 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(p2)
+    c3 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(c3)
+    p3 = layers.MaxPooling2D((2, 2))(c3)
+    
+    c4 = layers.Conv2D(512, (3, 3), activation='relu', padding='same')(p3)
+    c4 = layers.Conv2D(512, (3, 3), activation='relu', padding='same')(c4)
+    p4 = layers.MaxPooling2D((2, 2))(c4)
+    
+    # Bottleneck
+    c5 = layers.Conv2D(1024, (3, 3), activation='relu', padding='same')(p4)
+    c5 = layers.Conv2D(1024, (3, 3), activation='relu', padding='same')(c5)
+    
+    # Decoder
+    u6 = layers.UpSampling2D((2, 2))(c5)
+    u6 = layers.Conv2D(512, (3, 3), activation='relu', padding='same')(u6)
+    u6 = layers.concatenate([u6, c4])
+    c6 = layers.Conv2D(512, (3, 3), activation='relu', padding='same')(u6)
+    c6 = layers.Conv2D(512, (3, 3), activation='relu', padding='same')(c6)
+    
+    u7 = layers.UpSampling2D((2, 2))(c6)
+    u7 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(u7)
+    u7 = layers.concatenate([u7, c3])
+    c7 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(u7)
+    c7 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(c7)
+    
+    u8 = layers.UpSampling2D((2, 2))(c7)
+    u8 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(u8)
+    u8 = layers.concatenate([u8, c2])
+    c8 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(u8)
+    c8 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(c8)
+    
+    u9 = layers.UpSampling2D((2, 2))(c8)
+    u9 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(u9)
+    u9 = layers.concatenate([u9, c1])
+    c9 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(u9)
+    c9 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(c9)
+    
+    outputs = layers.Conv2D(3, (1, 1), activation='sigmoid')(c9)
+    
+    model = Model(inputs, outputs)
+    return model
+
+# Instantiate UNet model
+input_shape = (224, 224, 3)
+unet = build_unet(input_shape)
 
 # Define diffusion model parameters and training loop
 class DiffusionModel:
@@ -48,7 +104,12 @@ def save_samples(samples, output_dir):
         sample_path = os.path.join(output_dir, f'sample_{i+1}.png')
         tf.keras.preprocessing.image.save_img(sample_path, tf.squeeze(sample).numpy())
 
-diffusion = DiffusionModel(model)
+unet.load_weights("./checkpoints/ckpt.weights.h5")
+
+diffusion = DiffusionModel(unet)
+
+
+
 
 samples = diffusion.sample(num_samples=5)
 save_samples(samples, "./")
